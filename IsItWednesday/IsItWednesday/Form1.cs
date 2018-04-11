937ce96c9a063c2027fd2f6a3b203930cab73adf
Ardoriccardo00 <35687949+Ardoriccardo00@users.noreply.github.com>
2018-04-11 18:22:38 +0200

    finito

--- IsItWednesday/IsItWednesday/Form1.cs
+++ IsItWednesday/IsItWednesday/Form1.cs
@@ -7,11 +7,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Media;
 
 namespace IsItWednesday
 {
     public partial class Form1 : Form
     {
+        SoundPlayer player = new SoundPlayer(@"C:\Users\richi\source\repos\Is-it-Wednesday-my-dudes-\IsItWednesday\IsItWednesday\Resources\wed.wav");
         string ris;
         public Form1()
         {
@@ -26,38 +28,35 @@ namespace IsItWednesday
                 Res.Text = "";
                 timer1.Enabled = true;
                 yes.Visible = true;
-                do
-                {
-                    pictureBox1.Visible = true;
-                    pictureBox4.Visible = true;
-                    pictureBox5.Visible = true;
-                    pictureBox6.Visible = true;
-                    System.Threading.Thread.Sleep(5000);
-                    pictureBox1.Visible = false;
-                    pictureBox4.Visible = false;
-                    pictureBox5.Visible = false;
-                    pictureBox6.Visible = false;
-                } while (true);
+                player.Play();
 
                 } else { Res.Text = "No :("; }
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            //timer2.Enabled = true;
-            //pictureBox1.Visible = true;
-            //pictureBox4.Visible = true;
-            //pictureBox5.Visible = true;
-            //pictureBox6.Visible = true;
+            if (pictureBox1.Visible == true && pictureBox4.Visible == true && pictureBox5.Visible == true && pictureBox6.Visible == true)
+            {
+                pictureBox1.Visible = false;
+                pictureBox4.Visible = false;
+                pictureBox5.Visible = false;
+                pictureBox6.Visible = false;
+            }
+            else {
+                pictureBox1.Visible = true;
+                pictureBox4.Visible = true;
+                pictureBox5.Visible = true;
+                pictureBox6.Visible = true;
+            }
 
         }
 
         private void timer2_Tick(object sender, EventArgs e)
         {
-            //pictureBox1.Visible = false;
-            //pictureBox4.Visible = false;
-            //pictureBox5.Visible = false;
-            //pictureBox6.Visible = false;
+            pictureBox1.Visible = true;
+            pictureBox4.Visible = true;
+            pictureBox5.Visible = true;
+            pictureBox6.Visible = true;
         }
     }
 }
