a454c49f1ff95d3bd7358a8633229cb26fcdcb37
Ardoriccardo00 <35687949+Ardoriccardo00@users.noreply.github.com>
2018-04-11 18:06:29 +0200

    sicurezza

--- IsItWednesday/IsItWednesday/Form1.Designer.cs
+++ IsItWednesday/IsItWednesday/Form1.Designer.cs
@@ -28,21 +28,23 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
+            this.components = new System.ComponentModel.Container();
             this.Res = new System.Windows.Forms.Label();
+            this.yes = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.yes)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
             this.SuspendLayout();
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(220, 36);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(382, 42);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "IS IT WEDNESDAY?";
             // 
             // Res
             // 
@@ -50,43 +52,125 @@
             this.Res.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Res.Location = new System.Drawing.Point(55, 141);
             this.Res.Name = "Res";
-            this.Res.Size = new System.Drawing.Size(89, 73);
+            this.Res.Size = new System.Drawing.Size(0, 73);
             this.Res.TabIndex = 1;
-            this.Res.Text = "...";
+            // 
+            // yes
+            // 
+            this.yes.Image = global::IsItWednesday.Properties.Resources.yes_it_is;
+            this.yes.Location = new System.Drawing.Point(106, 169);
+            this.yes.Name = "yes";
+            this.yes.Size = new System.Drawing.Size(1132, 222);
+            this.yes.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.yes.TabIndex = 4;
+            this.yes.TabStop = false;
+            this.yes.Visible = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::IsItWednesday.Properties.Resources.iiwmd;
+            this.pictureBox2.Location = new System.Drawing.Point(106, 1);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(1132, 162);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
             // 
             // pictureBox1
             // 
             this.pictureBox1.Enabled = false;
             this.pictureBox1.Image = global::IsItWednesday.Properties.Resources.downloaaaaaaaaaaad;
-            this.pictureBox1.Location = new System.Drawing.Point(304, 257);
+            this.pictureBox1.Location = new System.Drawing.Point(106, 408);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(225, 225);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
             this.pictureBox1.TabIndex = 2;
             this.pictureBox1.TabStop = false;
+            this.pictureBox1.Visible = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Enabled = false;
+            this.pictureBox4.Image = global::IsItWednesday.Properties.Resources.downloaaaaaaaaaaad;
+            this.pictureBox4.Location = new System.Drawing.Point(382, 408);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(225, 225);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox4.TabIndex = 5;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Visible = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Enabled = false;
+            this.pictureBox5.Image = global::IsItWednesday.Properties.Resources.downloaaaaaaaaaaad;
+            this.pictureBox5.Location = new System.Drawing.Point(677, 408);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(225, 225);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox5.TabIndex = 6;
+            this.pictureBox5.TabStop = false;
+            this.pictureBox5.Visible = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Enabled = false;
+            this.pictureBox6.Image = global::IsItWednesday.Properties.Resources.downloaaaaaaaaaaad;
+            this.pictureBox6.Location = new System.Drawing.Point(972, 408);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(225, 225);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox6.TabIndex = 7;
+            this.pictureBox6.TabStop = false;
+            this.pictureBox6.Visible = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 500;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(965, 531);
+            this.BackgroundImage = global::IsItWednesday.Properties.Resources.windows_xp_bliss_wide;
+            this.ClientSize = new System.Drawing.Size(1364, 634);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.yes);
+            this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.Res);
-            this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.yes)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label Res;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox yes;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
     }
 }
 
--- IsItWednesday/IsItWednesday/Form1.cs
+++ IsItWednesday/IsItWednesday/Form1.cs
@@ -22,7 +22,42 @@ namespace IsItWednesday
         {
             DateTime d = DateTime.Now;
             ris = Convert.ToString(d.DayOfWeek);
-            if (ris == "Wednesday") { Res.Text = "It is wednesday my dudes!"; pictureBox1.Enabled = true; } else { Res.Text = "No :("; }
+            if (ris == "Wednesday") {
+                Res.Text = "";
+                timer1.Enabled = true;
+                yes.Visible = true;
+                do
+                {
+                    pictureBox1.Visible = true;
+                    pictureBox4.Visible = true;
+                    pictureBox5.Visible = true;
+                    pictureBox6.Visible = true;
+                    System.Threading.Thread.Sleep(5000);
+                    pictureBox1.Visible = false;
+                    pictureBox4.Visible = false;
+                    pictureBox5.Visible = false;
+                    pictureBox6.Visible = false;
+                } while (true);
+
+                } else { Res.Text = "No :("; }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //timer2.Enabled = true;
+            //pictureBox1.Visible = true;
+            //pictureBox4.Visible = true;
+            //pictureBox5.Visible = true;
+            //pictureBox6.Visible = true;
+
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            //pictureBox1.Visible = false;
+            //pictureBox4.Visible = false;
+            //pictureBox5.Visible = false;
+            //pictureBox6.Visible = false;
         }
     }
 }
